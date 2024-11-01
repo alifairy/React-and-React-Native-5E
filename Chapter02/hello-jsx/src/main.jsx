8e8ed241dee1923a4ebe89bdb0be2c30dee63266
胡睿毅 <huruiyi@outlook.com>
2024-11-01 16:32:24 +0800

    backup

--- Chapter02/hello-jsx/src/main.jsx
+++ Chapter02/hello-jsx/src/main.jsx
@@ -1,9 +1,17 @@
 import * as ReactDOM from "react-dom/client";
+import { decryptStr, encryptStr } from "@/enc.jsx";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
-root.render(
-  <p>
-    Hello, <strong>JSX</strong>
-  </p>
-);
+let encText = encryptStr("Hello World!世界你好!~~!@#$%^&*()_____+{}|[]:'<>?,./");
+let decText = decryptStr(encText);
+
+root.render(<p>
+  Hello, <strong>JSX</strong>
+  <label>
+    加密： { encText }
+  </label>
+  <label>
+    解密： { decText }
+  </label>
+</p>);
---
+++ Chapter02/hello-jsx/src/utils.jsx
@@ -0,0 +1,27 @@
+/**
+ * 判断是否为数字类型
+ * @param num 需要进行判断的变量
+ * @returns {boolean} true：数字类型；false：非数字类型
+ */
+export function isNumber(num) {
+    return !(num === null) && !(num === undefined) && typeof(num) === 'number'
+}
+/**
+ * 判断是否为字符串类型
+ * @param str 需要判断的变量
+ * @returns {boolean} true：字符串类型；false：非字符串类型
+ */
+export function isString(str) {
+    return !(str === null) && !(str === undefined) && typeof(str) === 'string'
+}
+
+/**
+ * 判断一个字符串是否为空。
+ * 当传入的参数为 null 或 undefined 或 不为字符串 或 字符串的长度小于等于0，则该字符串为空；
+ * 否则，字符串不为空
+ * @param str 需要进行判断的字符串
+ * @returns {boolean} true：空；false：非空
+ */
+export function stringIsNull(str) {
+    return !(isString(str)) || str.length <= 0
+}
